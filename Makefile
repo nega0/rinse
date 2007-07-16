668ed2c96f2e25306dc4dbcd0a38deb9fd6a22f9
steve <none@none>
2007-07-16 00:15:42 +0000

    2007-07-16 00:15:42 by steve Initial revision

---
+++ Makefile
@@ -0,0 +1,105 @@
+#
+#  Makefile for rinse, the RPM installation entity
+#
+# Steve
+# --
+# $Id: Makefile,v 1.1.1.1 2007-07-16 00:15:42 steve Exp $
+#
+
+
+#
+#  Only used to build distribution tarballs.
+#
+DIST_PREFIX = ${TMP}
+VERSION     = 0.2
+BASE        = rinse
+PREFIX      = 
+
+
+#
+#  Report on targets.
+#
+default:
+	@echo " The following targets are available:"
+	@echo " "
+	@echo "  clean        - Remove editor backups"
+	@echo "  install      - Install to ${PREFIX}"
+	@echo "  test         - Run the tests"
+	@echo "  test-verbose - Run the tests, verbosely"
+	@echo "  uninstall    - Uninstall from ${PREFIX}"
+	@echo " "
+
+
+#
+#  Clean edited files.
+#
+clean:
+	@find . -name '*~' -delete
+	@find . -name '.#*' -delete
+
+
+
+#
+#  Make sure our scripts are executable.
+#
+fixupperms:
+	for i in scripts/*/*.sh; do chmod 755 $$i; done
+
+
+#
+#  Install software
+#
+install: fixupperms
+	mkdir -p ${PREFIX}/etc/rinse
+	mkdir -p ${PREFIX}/usr/bin
+	mkdir -p ${PREFIX}/usr/lib/rinse
+	mkdir -p ${PREFIX}/var/cache/rinse
+	cp bin/rinse ${PREFIX}/usr/bin/
+	chmod 755 ${PREFIX}/usr/bin/rinse
+	cp etc/*.packages ${PREFIX}/etc/rinse
+	cp etc/*.conf     ${PREFIX}/etc/rinse
+	for i in scripts/*/; do mkdir -p ${PREFIX}/usr/lib/rinse/`basename $$i`; cp $$i/*.sh  ${PREFIX}/usr/lib/rinse/`basename $$i` ; done
+	if [ -d /etc/bash_completion.d/ ]; then cp misc/rinse  /etc/bash_completion.d/ ; fi
+
+
+#
+#  Make a new release tarball, and make a GPG signature.
+#
+release: clean
+	rm -rf $(DIST_PREFIX)/$(BASE)-$(VERSION)
+	rm -f $(DIST_PREFIX)/$(BASE)-$(VERSION).tar.gz
+	cp -R . $(DIST_PREFIX)/$(BASE)-$(VERSION)
+	find  $(DIST_PREFIX)/$(BASE)-$(VERSION) -name "CVS" -print | xargs rm -rf
+	rm -rf $(DIST_PREFIX)/$(BASE)-$(VERSION)/debian
+	cd $(DIST_PREFIX) && tar --exclude=.cvsignore -cvf $(DIST_PREFIX)/$(BASE)-$(VERSION).tar $(BASE)-$(VERSION)/
+	gzip $(DIST_PREFIX)/$(BASE)-$(VERSION).tar
+	mv $(DIST_PREFIX)/$(BASE)-$(VERSION).tar.gz .
+	rm -rf $(DIST_PREFIX)/$(BASE)-$(VERSION)
+	gpg --armour --detach-sign $(BASE)-$(VERSION).tar.gz
+
+
+#
+#  Run the test suite.  (Minimal.)
+#
+test:
+	prove --shuffle tests/
+
+
+#
+#  Run the test suite verbosely.  (Minimal.)
+#
+test-verbose:
+	prove --shuffle --verbose tests/
+
+
+#
+#  Remove the software.
+#
+uninstall:
+	rm -f  ${PREFIX}/usr/bin/rinse
+	rm -f  ${PREFIX}/etc/rinse/*.conf
+	rm -f  ${PREFIX}/etc/rinse/*.packages
+	rm -rf ${PREFIX}/var/cache/rinse
+	rm -rf ${PREFIX}/usr/lib/rinse
+	rm -f  ${PREFIX}/etc/bash_completion.d/rinse
+
---
+++ scripts/centos-5/post-install.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+#
+#
+
+
+prefix=$1
+
+if [ ! -d "${prefix}" ]; then
+  echo "Serious error - the named directory doesn't exist."
+  exit
+fi
+
+
+
+#
+#  1.  Make sure there is a resolv.conf file present, such that
+#     DNS lookups succeed.
+#
+echo "  Creating resolv.conf"
+if [ ! -d "${prefix}/etc/" ]; then
+    mkdir -p "${prefix}/etc/"
+fi
+cp /etc/resolv.conf "${prefix}/etc/"
+
+
+#
+#  2.  Setup yum.conf
+#
+cat >>${prefix}/etc/yum.conf <<EOF
+[base]
+name=CentOS-5 - Base
+mirrorlist=http://mirrorlist.centos.org/?release=5&arch=i386&repo=os
+baseurl=http://mirror.centos.org/centos/5/os/i386/
+gpgcheck=1
+gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-centos4
+priority=1
+protect=1
+
+#released updates 
+[update]
+name=CentOS-5 - Updates
+mirrorlist=http://mirrorlist.centos.org/?release=5&arch=i386&repo=updates
+baseurl=http://mirror.centos.org/centos/5/updates/i386/
+gpgcheck=1
+gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-centos4
+priority=1
+protect=1
+
+#packages used/produced in the build but not released
+[addons]
+name=CentOS-5 - Addons
+mirrorlist=http://mirrorlist.centos.org/?release=5&arch=i386&repo=addons
+baseurl=http://mirror.centos.org/centos/5/addons/i386/
+gpgcheck=1
+gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-centos4
+priority=1
+protect=1
+
+#additional packages that may be useful
+[extras]
+name=CentOS-5 - Extras
+mirrorlist=http://mirrorlist.centos.org/?release=5&arch=i386&repo=extras
+baseurl=http://mirror.centos.org/centos/5/extras/i386/
+gpgcheck=1
+gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-centos4
+priority=1
+protect=1
+
+#additional packages that extend functionality of existing packages
+[centosplus]
+name=CentOS-5 - Plus
+mirrorlist=http://mirrorlist.centos.org/?release=5&arch=i386&repo=centosplus
+baseurl=http://mirror.centos.org/centos/5/centosplus/i386/
+gpgcheck=1
+enabled=0
+gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-centos4
+priority=2
+protect=1
+
+#contrib - packages by Centos Users
+[contrib]
+name=CentOS-5 - Contrib
+mirrorlist=http://mirrorlist.centos.org/?release=5&arch=i386&repo=contrib
+baseurl=http://mirror.centos.org/centos/5/contrib/i386/
+gpgcheck=1
+enabled=0
+gpgkey=http://mirror.centos.org/centos/RPM-GPG-KEY-centos4
+priority=2
+protect=1
+
+EOF
+
+
+#
+#  3.5 BUGFIX:
+echo "BUGFIX"
+mkdir -p ${prefix}usr/lib/python2.4/site-packages/urlgrabber.skx
+for i in ${prefix}/usr/lib/python2.4/site-packages/urlgrabber/keepalive.*; do
+    mv $i ${prefix}usr/lib/python2.4/site-packages/urlgrabber.skx/
+done
+
+#
+#
+#  4.  Run "yum install yum".
+#
+echo "  Mounting /proc"
+if [ ! -d "${prefix}/proc" ]; then
+    mkdir -p "${prefix}/proc"
+fi
+mount -o bind /proc ${prefix}/proc
+
+echo "  Bootstrapping yum"
+chroot ${prefix} /usr/bin/yum -y install yum passwd 2>/dev/null
+
+
+#
+#  5.  Clean up
+#
+echo "  Cleaing up"
+chroot ${prefix} /usr/bin/yum clean all
+umount ${prefix}/proc
+
+
+#
+#  6.  Remove the .rpm files from the prefix root.
+#
+echo "  Final tidy..."
+for i in ${prefix}/*.rpm; do 
+    rm -f $i
+done
+find ${prefix} -name '*.rpmorig' -delete
+find ${prefix} -name '*.rpmnew' -delete
