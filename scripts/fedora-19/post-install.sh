1aa89ab522d1b8150bd313203208e45221068653
nega <nega@icecube.umd.edu>
2014-05-05 16:30:44 -0400

    let's copy out of the correct directory

--- scripts/fedora-19/post-install.sh
+++ scripts/fedora-19/post-install.sh
@@ -89,7 +89,7 @@ echo "  Priming the yum cache"
 if [ ! -d "${prefix}/var/cache/yum/core/packages/" ]; then
     mkdir -p ${prefix}/var/cache/yum/core/packages
 fi
-cp /var/cache/rinse/fedora-18.$ARCH/* ${prefix}/var/cache/yum/core/packages/
+cp /var/cache/rinse/fedora-19.$ARCH/* ${prefix}/var/cache/yum/core/packages/
 
 echo "  Bootstrapping yum"
 chroot ${prefix} /usr/sbin/ldconfig
